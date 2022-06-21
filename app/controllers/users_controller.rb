f9d6396b9ee3a555805bec82f32d2461405b8e80
MizuhoMatsushima <105145800+MizuhoMatsushima@users.noreply.github.com>
2022-06-21 05:02:17 +0000

    [Finish]4_Bookers2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,10 +16,9 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
-    if @user ==current_user
-      render "edit"
+    if @user == current_user
     else
-      redirect_to edit_user_path(current_user.id)
+      redirect_to user_path(current_user)
     end
   end
 
